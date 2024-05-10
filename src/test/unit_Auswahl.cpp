142effb395fd066511d4b7be3c870a4e33e79c4d
SoGam34 <117866365+SoGam34@users.noreply.github.com>
2024-05-10 13:12:35 +0200

    Test: Anpassen und hinzufügen von Test

---
+++ src/test/unit_Auswahl.cpp
@@ -0,0 +1,22 @@
+/**
+ * @file unit_Auswahl.cpp
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2024-05-10
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+#define BOOST_TEST_MODULE timer
+#include <boost/test/included/unit_test.hpp>
+#include "../Auswahl.h"
+#include "../Data/Data.h"
+
+BOOST_AUTO_TEST_SUITE( auswahl )
+
+BOOST_AUTO_TEST_CASE( suche_Einsezbar )
+{ 
+}
+
+BOOST_AUTO_TEST_SUITE_END()
