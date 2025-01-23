f2f1190ce3faf38a1fb3aa0bb2d025bf13283bb8
SoGam34 <117866365+SoGam34@users.noreply.github.com>
2025-01-23 23:59:39 +0000

    (CI-Pipline): Formating Codebase

--- src/Rendern/cli/Cli.cpp
+++ src/Rendern/cli/Cli.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 #include <utility>
 #include "../View.h"
-    
+
 Cli::Cli(std::shared_ptr<Data> data) : Eingabe(0), View(std::move(data)) {}
 
 bool Cli::getSpielIstAktiv() const {
